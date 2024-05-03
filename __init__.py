b86756311ef2b7d8cd357c99f645e375f07e2886
yacineberkani <159167247+yacineberkani@users.noreply.github.com>
2024-05-03 12:00:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,3 +5,5 @@ import os
 from langdetect import detect
 from tqdm import tqdm
 import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
